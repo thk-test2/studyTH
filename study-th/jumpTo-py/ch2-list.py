d13478ee8bbe5125d007b1e87331b9e75f631651
Taehyun Kyong <ohyeaha@hanmail.net>
2019-06-19 23:59:11 +0900

    [studyTH] Study jumpToPy chapter 2
    :Release Notes:
    [studyTH] Study jumpToPy chapter 2
    
    :Detailed Notes:
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-9999] Summary

---
+++ study-th/jumpTo-py/ch2-list.py
@@ -0,0 +1,18 @@
+
+a = ['Life', 'is', 'too', 'short']
+
+print(" ".join(a))
+
+a = [1, 3, 5, 4, 2]
+
+a.sort()
+a.reverse()
+print(a)
+
+# https://wikidocs.net/14
+
+t1 = (1,2,3)
+
+print(t1 + (4,))
+
+# https://wikidocs.net/15
---
+++ study-th/jumpTo-py/ch2-string.py
@@ -0,0 +1,32 @@
+a = "a:b:c:d"
+
+d = a.replace(":", "#")
+print(d)
+
+b = a.split(':')
+
+c ="#".join(b)
+
+print(c)
+
+https://wikidocs.net/13
+
+# 문자열 포매팅
+문자열 포매팅 따라 하기
+문자열 포맷 코드
+포맷 코드와 숫자 함께 사용하기
+format 함수를 이용한 포매팅
+f 문자열 포매팅
+
+# 문자열 관련 함수들
+문자 개수 세기(count)
+위치 알려주기1(find)
+위치 알려주기2(index)
+문자열 삽입(join)
+소문자를 대문자로 바꾸기(upper)
+대문자를 소문자로 바꾸기(lower)
+왼쪽 공백 지우기(lstrip)
+오른쪽 공백 지우기(rstrip)
+양쪽 공백 지우기(strip)
+문자열 바꾸기(replace)
+문자열 나누기(split)
