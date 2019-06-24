cbcc2a881694cc891a737b715f06aa8b67bb0425
Taehyun Kyong <ohyeaha@hanmail.net>
2019-06-24 23:46:27 +0900

    [studyTH] baeckjoon python solving
    :Release Notes:
    [studyTH] baeckjoon python solving
    
    :Detailed Notes:
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-9999] Summary

---
+++ study-th/baekjoon-py/2562.main.py
@@ -0,0 +1,13 @@
+numbers = []
+max_num = 0
+idx = 0
+
+for i in range(9):
+    number = int(input())
+    numbers.append(number)
+    max_num = max(numbers) 
+    if max_num == number:
+        idx = i + 1
+
+print(max_num)
+print(idx)
---
+++ study-th/baekjoon-py/2884.main.py
@@ -0,0 +1,9 @@
+H, M = map(int, input().split())
+
+if M >= 45:
+    print(H, M-45)
+else:
+    if H > 0:
+        print(H-1, M+60-45)
+    else:
+        print(23, M+60-45)
