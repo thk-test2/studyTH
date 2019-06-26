adcc9a90f2c486261b3035b8998cb34ae14a6c30
Taehyun Kyong <ohyeaha@hanmail.net>
2019-06-26 23:56:03 +0900

    [studyTH] baekjoon python solving
    :Release Notes:
    [studyTH] baekjoon python solving
    
    :Detailed Notes:
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-9999] Summary

---
+++ study-th/baekjoon-py/10872.main.py
@@ -0,0 +1,9 @@
+
+def fac(n):
+    if (n==0 or n==1):
+        return 1
+    return n*fac(n-1)
+
+n = int(input())
+result = fac(n)
+print(result)
---
+++ study-th/baekjoon-py/15596.main.py
@@ -0,0 +1,9 @@
+
+def solve(a: list) -> int:
+    sum = 0
+    for i in a:
+        sum += i
+    return sum
+
+result = solve([1, 2, 3])
+print(result)
