48b408657214e53fdbb1751064058bc0817ed2f4
Taehyun Kyong <th.kyong@lge.com>
2019-09-18 15:33:59 +0900

    [studyTH] mooc-py
    :Release Notes:
    [studyTH] mooc-py
    
    :Detailed Notes:
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-9999] Summary

---
+++ study-th/mooc-py/3.1.py
@@ -0,0 +1,13 @@
+
+print(25 // 10 ** 2 + 1)
+
+scores = { 'Korean': 80, 'Math': 90, 'English': 100}
+print(scores['Math'])
+
+word = 'Python'
+print(word[2:4])
+
+if 'on' in word:
+    print(True)
+else:
+    print(False)
