4510f38da7cc13bb57fabf3fd635911205583840
Taehyun Kyong <th.kyong@lge.com>
2019-11-12 08:57:15 +0900

    [cpp] baekjoon 1932 - timeover
    :Release Notes:
    [cpp] baekjoon 1932 - timeover
    
    :Detailed Notes:
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [GF-9999] Summary

--- studyTH/baekjoon-cpp/1932.main.cpp
+++ studyTH/baekjoon-cpp/1932.main.cpp
@@ -1,33 +1,54 @@
 
 #include <iostream>
-#include <vector>
+#include <array>
 #include <string>
 #include <sstream>
 
-int triangle(int part_sum, int i, int j) {
+// std::vector<std::pair<int, int>> num_vec;
+std::array<std::array<int, 500>, 500> num_vec;
+int height=0;
+std::array<int, 500> result;
 
+int triangle(int part_sum, int column, int row) {
+    if (column == height)
+        return part_sum;
+
+    part_sum = part_sum + num_vec[column][row];
+    std::cout << part_sum << std::endl;
+    return std::max(triangle(part_sum, column+1, row), triangle(part_sum, column+1, row+1));
 }
 
 int main() {
 
-    int height=0;
+    // int height=0;
     std::cin >> height;
     std::cin >> std::ws;
 
     std::string line;
-    std::vector<std::pair<int, int>> line_num;
 
     for (int i = 0; i< height ; i++) {
         getline(std::cin, line);
         std::stringstream ss(line);
         int token;
-        
+        int j=0; 
         while(ss >> token) {
-            line_num.emplace_back(i, token);
+            num_vec[i][j] = token;
+            j++;
+            // num_vec.emplace_back(i, token);
         }
     }
 
-    for( const auto & itr : line_num ) {
-        std::cout << itr.first << " " << itr.second << std::endl;
-    }
+    // for( const auto & itr : num_vec ) {
+    //     std::cout << itr.first << " " << itr.second << std::endl;
+    // }
+
+    int sum = triangle(0, 0, 0);
+    std::cout << sum << std::endl;
+
+    // int sum2 = 0;
+    // int j=0;
+    // for (int i = 0; i < height ; i++) {
+    //     sum2 = sum2 + num_vec[i][j];
+    //     std::max(sum2+num_vec[i+1][j], sum2+num_vec[i+1][j+1]);
+    // }
 }
