d2819454124d2cdfb3c95dffcf4be34852e1e251
Taehyun Kyong <ohyeaha@hanmail.net>
2020-05-26 00:05:59 +0900

    [toy] Http Server - 1

---
+++ studyTH/toyProject/01_httpServer/test.h
@@ -0,0 +1,18 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+
+class TestClass
+{
+public:
+    void print() {
+        std::cout << "I want to make a HTTP server" << std::endl;
+    }
+    void update() {
+        std::cout << "Hello World!" << std::endl;
+    }
+
+};
+
+#endif
