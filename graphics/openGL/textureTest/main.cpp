4c17570795e8e4d599feb8886c102c2653a12f06
Taehyun Kyong <taehyun.kyong@qt.io>
2021-01-19 23:58:14 +0900

    [openGL] add texture example

---
+++ graphics/openGL/textureTest/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cmath>
+#include <unistd.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "Shader.h"
+
+using namespace std;
+
+float vertices[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+};
+
+float texCoords[] = {
+    0.0f, 0.0f,  // lower-left corner
+    1.0f, 0.0f,  // lower-right corner
+    0.5f, 1.0f   // top-center corner
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialise GLAD" << endl;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+//    char cwd[1024];
+//    getcwd(cwd, sizeof(cwd));
+//    string s0(cwd);
+//    string s1(s0+"shader.vs");
+//    string s2(s0+"shader.fs");
+//    cout << s1;
+
+    Shader ourShader("/home/parallels/projects/study/studyTH/graphics/openGL/textureTest/shader.vs",
+                     "/home/parallels/projects/study/studyTH/graphics/openGL/textureTest/shader.fs");
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //render loop
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) +0.5f;
+        int vertexColorLocation = glGetUniformLocation(ourShader.getID(), "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
