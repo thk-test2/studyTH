a6aa61b6ffb4a735ca0ee1094acbab5ceb4af9d4
Taehyun Kyong <taehyun.kyong@qt.io>
2021-04-30 23:19:33 +0900

    [cpp][algorithm] improving string divider

--- algorithm/basic/01_string_divide/main.cpp
+++ algorithm/basic/01_string_divide/main.cpp
@@ -9,54 +9,58 @@ int solution(string &S, int K) {
     if (K >= S.length())
         return 1;
 
-    vector<string> result;
     int previous = 0;
     int current = 0;
 
-    current = S.find(' ');
-    string substring;
-    while(1) {
-        string temp = S.substr(previous, current - previous);
-        if ( (substring.length() + temp.length() + 1) <= K) {
-            if (substring.empty()) {
-                substring = temp;
-            } else {
-                substring.append(" ");
-                substring.append(temp);
-                result.push_back(substring);
-                substring.clear();
+    int count = 0;
+    string mainStr, temp;
+    while(current != string::npos) {
+        current = S.find(' ', previous);
+
+        temp = S.substr(previous, current - previous);
+        if (mainStr.empty()){
+            if (temp.length() <= K)
+            {
+                mainStr.append(temp);
+                count++;
+            }
+        } else {
+            if (mainStr.length()+temp.length() < K)
+            {
+                mainStr.append(" ");
+                mainStr.append(temp);
+            } else if (temp.length() <= K) {
+                mainStr.clear();
+                mainStr.append(temp);
+                count++;
             }
         }
+        cout << mainStr << ", ";
         previous = current + 1;
-        current = S.find(' ', previous);
-        if (current == string::npos) {
-            if(!substring.empty())
-                result.push_back(substring);
-            break;
-        }
-    }
-    string temp2 = S.substr(previous, current - previous);
-    if (temp2.length() <= K)
-        result.push_back(temp2);
 
-    if (result.empty())
-        return -1;
-
-    for (const auto& it: result)
-    {
-        cout << it << ", ";
+//        if (sub.length() <= K) {
+//            if (!mainStr.empty()) {
+//                mainStr.clear();
+//            }
+//            mainStr.append(sub);
+//            count++;
+//        } else if ( mainStr.length() + sub.length() < K) {
+//            mainStr.append(" ");
+//            mainStr.append(sub);
+//        }
+//        cout << mainStr << ", ";
     }
 
-    return result.size();
+    return count;
 }
 
 int main()
 {
     string s("Hello Iam Taehyun");
-    cout << solution(s, 7) << endl;
+    cout << solution(s, 8) << endl;
 
-//    string s1("Codility We test coders");
-//    cout << solution(s1, 14) << endl;
+    string s1("Codility We test coders");
+    cout << solution(s1, 14) << endl;
 
 //    string s2("To crop or not to crop");
 //    cout << solution(s2, 21) << endl;
