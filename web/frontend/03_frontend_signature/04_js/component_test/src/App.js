825648ccebee7f81d0b275b1f00c3f7c677e30d5
Taehyun Kyong <ohyeaha@hanmail.net>
2023-03-16 23:02:53 +0900

    [web] Make and test child component
    Make and test child component

--- web/frontend/03_frontend_signature/04_js/component_test/src/App.js
+++ web/frontend/03_frontend_signature/04_js/component_test/src/App.js
@@ -1,5 +1,6 @@
 
 import { Component } from "./core/heropy";
+import FruitItem from "./components/FruitItem";
 
 export default class App extends Component {
   constructor() {
@@ -18,12 +19,18 @@ export default class App extends Component {
 
     this.el.innerHTML = /* html */ `
       <h1>Fruits</h1>
-      <ul>
-        ${this.state.fruits
-          .filter(fruit => fruit.price < 3000)
-          .map(fruit => `<li>${fruit.name}</li>`)
-          .join('')}
-      </ul>
+      <ul></ul>
     `
+
+    const ulEl = this.el.querySelector('ul');
+    ulEl.append(...this.state.fruits
+      .filter(fruit => fruit.price < 3000)
+      .map(fruit => new FruitItem({
+        props: {
+          name: fruit.name,
+          price: fruit.price
+        }
+      }).el)
+    );
   }
 }
--- web/frontend/03_frontend_signature/04_js/component_test/src/core/heropy.js
+++ web/frontend/03_frontend_signature/04_js/component_test/src/core/heropy.js
@@ -4,10 +4,12 @@ export class Component {
   constructor(payload = {}) {
     const {
       tagName = 'div',
-      state = {}
+      state = {},
+      props = {}
     } = payload;
     this.el = document.createElement(tagName);
     this.state = state;
+    this.props = props;
     this.render();
   }
   render() {
