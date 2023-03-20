043cc2a558faf790d011bb6f2197d89fe3f576d7
Taehyun Kyong <ohyeaha@hanmail.net>
2023-03-20 22:26:00 +0900

    [web] Movie app - init project
    Movie app - init project

---
+++ web/frontend/03_frontend_signature/04_js/movie-app/src/core/heropy.js
@@ -0,0 +1,87 @@
+
+//// Component ////
+export class Component {
+  constructor(payload = {}) {
+    const {
+      tagName = 'div',
+      state = {},
+      props = {}
+    } = payload;
+    this.el = document.createElement(tagName);
+    this.state = state;
+    this.props = props;
+    this.render();
+  }
+  render() {
+    // ...
+  }
+}
+
+
+//// Router ////
+function routeRender(routes) {
+  if (!location.hash) {
+    history.replaceState(null, '', '/#/');
+  }
+
+  const routerView = document.querySelector('router-view');
+  // http://localhost:1234/#/about
+  // #/about?name=heropy
+  const [hash, queryString = ''] = location.hash.split('?');
+
+  // a=123&b=456
+  // ['a=123', 'b=456']
+  // { a: '123', b: '456' }
+  const query = queryString
+    .split('&')
+    .reduce((acc, cur) => {
+      const [key, value] = cur.split('=');
+      acc[key] = value;
+      return acc;
+  }, {});
+  history.replaceState(query, '');
+
+  const currentRoute = routes.find(route => new RegExp(`${route.path}/?$`).test(hash));
+  routerView.innerHTML = '';
+  routerView.append(new currentRoute.component().el);
+
+  window.scrollTo(0, 0);
+}
+export function createRouter(routes) {
+  return function() {
+    window.addEventListener('popstate', () => {
+      routeRender(routes);
+    })
+    routeRender(routes);
+  }
+}
+
+
+//// Store ////
+export class Store {
+  constructor(state) {
+    this.state = {};
+    this.observers = {};
+    for (const key in state) {
+      Object.defineProperty(this.state, key, {
+        get: () => state[key], // state['message']
+        set: val => {
+          state[key] = val;
+          // this.observers['message']()
+          // this.observers[key]();
+          this.observers[key].forEach(observer => observer(val));
+        }
+      });
+    }
+  }
+  subscribe(key, cb) {
+    // this.observers['message'] = () => {}
+    // { message: () => {} }
+    // { message: [() => {}, () => {}, ...]}
+
+    // { message: [cb1, cb2, cb3, ...] }
+    Array.isArray(this.observers[key])
+      ? this.observers[key].push(cb)
+      : this.observers[key] = [cb];
+  }
+}
