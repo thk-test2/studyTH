d8ef91dae2871ea65a3a45c47e6c0d9485fb75c8
Taehyun Kyong <ohyeaha@hanmail.net>
2023-03-26 18:36:52 +0900

    [web][js] Movie - Show more, loading animation
    Movie - Show more, loading animation

--- web/frontend/03_frontend_signature/04_js/movie-app/src/store/movie.js
+++ web/frontend/03_frontend_signature/04_js/movie-app/src/store/movie.js
@@ -3,19 +3,24 @@ import { Store } from "../core/heropy";
 const store = new Store({
   searchText: '',
   page: 1,
-  movies: []
+  pageMax: 1,
+  movies: [],
+  loading: false
 })
 
 export default store;
 export const searchMovies = async page => {
-  if(page === 1) {
-    store.state.page = 1;
+  store.state.loading = true;
+  store.state.page = page;
+  if (page === 1) {
     store.state.movies = [];
   }
   const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
-  const { Search } = await res.json();
+  const { Search, totalResults } = await res.json();
   store.state.movies = [
     ...store.state.movies,
     ...Search
   ];
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  store.state.loading = false;
 }
