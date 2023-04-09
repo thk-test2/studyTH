f6ebf943107c2e85e9c1eaf28a48e6fbde17f486
Taehyun Kyong <ohyeaha@hanmail.net>
2023-04-09 18:27:20 +0900

    [web][js] Movie - TheFooter, About component
    Movie - TheFooter, About component

---
+++ web/frontend/03_frontend_signature/04_js/movie-app/src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'HEROPY / ParkYoungWoong',
+  email: 'thesecon@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/ParkYoungWoong',
+  repository: 'https://github.com/ParkYoungWoong/vanillajs-movie-app'
+})
