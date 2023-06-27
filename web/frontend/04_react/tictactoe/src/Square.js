28743702d2329280dad17c8f725ce16e041fb7cf
Taehyun Kyong <ohyeaha@hanmail.net>
2023-06-27 22:17:59 +0900

    [react] Seperate to Components

---
+++ web/frontend/04_react/tictactoe/src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
