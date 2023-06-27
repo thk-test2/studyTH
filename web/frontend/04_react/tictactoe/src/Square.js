5537e4300c7c03b8045e06080d9d3d448479d29e
Taehyun Kyong <ohyeaha@hanmail.net>
2023-06-27 22:44:46 +0900

    [react] Change to function component

--- web/frontend/04_react/tictactoe/src/Square.js
+++ web/frontend/04_react/tictactoe/src/Square.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-export default class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" 
-        onClick= {() => { this.props.onClick() }}>
-        { this.props.value }
-      </button>
-    )
-  }
+const Square = ({ onClick, value}) => {
+  return (
+    <button className="square" 
+      onClick= {onClick}>
+      { value }
+    </button>
+  )
 }
+
+export default Square;
