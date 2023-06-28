44f805c9f99cae6461894fd8fedb24e532520b46
Taehyun Kyong <ohyeaha@hanmail.net>
2023-06-28 22:04:52 +0900

    [react] Decide winner

--- web/frontend/04_react/tictactoe/src/Board.js
+++ web/frontend/04_react/tictactoe/src/Board.js
@@ -4,13 +4,51 @@ import './index.css';
 import Square from './Square';
 
 const Board = () => {
-
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (squares[a] && squares[a] === squares[b]
+        && squares[a] === squares[c]) {
+          return squares[a];
+        }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  console.log(winner);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
+    // setXIsNext(prev => !prev)
   }
 
   const renderSquare = (i) => {
@@ -20,7 +58,7 @@ const Board = () => {
   
   return (
     <div>
-      <div className='status'>Next player: X, O</div>
+      <div className='status'>{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
