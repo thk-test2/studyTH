b5140004c8426f9785b9c343d012bfb4f22e15e7
Taehyun Kyong <ohyeaha@hanmail.net>
2023-06-30 13:25:40 +0900

    [react] Finish tictactoe

--- web/frontend/04_react/tictactoe/src/App.js
+++ web/frontend/04_react/tictactoe/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
   const calculateWinner = (squares) => {
@@ -27,7 +28,7 @@ function App() {
     return null;
   }
 
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
 
   let status;
@@ -38,16 +39,21 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
 
     if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
 
     newSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, { squares: newSquares }]);
+
     setXIsNext(!xIsNext);
-    // setXIsNext(prev => !prev)
+    // setHistory([...history, { squares: newSquares }]);
+    setHistory([...newHistory, { squares: newSquares }]);
+
+    setStepNumber(newHistory.length);
   }
 
   const moves = history.map((step, move) => {
@@ -56,10 +62,15 @@ function App() {
     'Go to game start';
     return (
       <li key={move}>
-        <button>{desc}</button>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     )
   })
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }
 
   return (
     <div className="game">
