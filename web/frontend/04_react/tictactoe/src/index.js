ed000cb07ef848fdf976a82608596b9d7ad15ddb
Taehyun Kyong <ohyeaha@hanmail.net>
2023-06-30 11:57:00 +0900

    [react] Add App component

--- web/frontend/04_react/tictactoe/src/index.js
+++ web/frontend/04_react/tictactoe/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './Board.js'
+import App from './App';
 
 class Game extends React.Component {
   render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          game-info
-        </div>
-      </div>
-    );
+    return <div>
+      <App />
+    </div>
   }
 }
 
